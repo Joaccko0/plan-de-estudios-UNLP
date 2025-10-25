8d9c8f0d2b4d45f3fef0578e7c614b592f87c122
Joaccko0 <joaquinrd0@gmail.com>
2025-10-25 16:49:40 -0300

    Arreglos

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,17 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  tauri::Builder::default()
-    .setup(|app| {
-      if cfg!(debug_assertions) {
-        app.handle().plugin(
-          tauri_plugin_log::Builder::default()
-            .level(log::LevelFilter::Info)
-            .build(),
-        )?;
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_log::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![open_url])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn open_url(url: String) {
+    println!("Attempting to open URL: {}", url);
+    if let Err(e) = open::that(&url) {
+        eprintln!("Failed to open URL {}: {}", url, e);
+    }
 }
